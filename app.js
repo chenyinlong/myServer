b64f374e2ab2249ca1b9fc78b66f49d93cb85e8a
chenyinlong <985350503@qq.com>
2017-05-22 14:07:13 +0800

    测试git-plus

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,10 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
+var app = express(); // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
